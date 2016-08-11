419890ba0cdc2b2ad825cf3b861d79d89230c744
ibrewdudes <ibrewdudes@gmail.com>
2016-08-11 18:49:32 +0200

    Añadido combo para elegir factor de forma del lupulo

--- controller.js
+++ controller.js
@@ -9,16 +9,23 @@ angular.module('app.controllers', []).controller('appController', funcionControl
 //
 //--------------------------------------------------------------------------------
 function funcionController($scope, $http, IbusFactory){
-	//$scope.ibus = IbusFactory.ibus();
+
 	$scope.ibus = {
-		tiempo_hervor: 0,
-		tipo_lupulo: 0,
-		gramos_lupulo: 0,
-		alfa_acidos: 0,
-		litros_mosto_post_hervor: 0,
-		densidad_mosto_post_hervor: 0,
-		ibus_calculados: 0,
+		tiempo_hervor: '',
+		tipo_lupulo: {id: 0, descripcion: 'Flor'},
+		gramos_lupulo: '',
+		alfa_acidos: '',
+		litros_mosto_post_hervor: '',
+		densidad_mosto_post_hervor: '',
+		ibus_calculados: 0
 	};
+
+	// Para mostrar el combo con los tipos de lúpulo (flor/pellet)
+  $scope.tipos_lupulo = [
+		{id: 0, descripcion: 'Flor'},
+		{id: 1, descripcion: 'Pellet'}
+	];
+
 
 	//--------------------------------------------------------------------------------
   // Función para limpiar los datos del formulario
@@ -26,13 +33,15 @@ function funcionController($scope, $http, IbusFactory){
 	$scope.reset = function(){
 		$scope.ibus = {
 			tiempo_hervor: 60,
-			tipo_lupulo: 0,
+			tipo_lupulo: {id: 0, descripcion: 'Flor'},
 			gramos_lupulo: 15,
 			alfa_acidos: 7.2,
 			litros_mosto_post_hervor: 20,
 			densidad_mosto_post_hervor: 1060,
 			ibus_calculados: 0,
 		};
+
+		$scope.tipo_lupulo_seleccionado = {id: 0, descripcion: 'Flor'};
 	}
 
 	//--------------------------------------------------------------------------------
@@ -40,8 +49,16 @@ function funcionController($scope, $http, IbusFactory){
 	//--------------------------------------------------------------------------------
 	$scope.calcularIbus = function(misDatos){
 		// Llamamos al servicio que calcula los ibus
+//alert("Tiempo: " + misDatos.tiempo_hervor);
+//alert("Tipo lúpulo: " + misDatos.tipo_lupulo.id);
+//alert("Gramos: " + misDatos.gramos_lupulo);
+//alert("Alfa ácidos: " + misDatos.alfa_acidos);
+//alert("Mosto post-hervor: " + misDatos.litros_mosto_post_hervor);
+//alert("Densidad mosto: "+ misDatos.densidad_mosto_post_hervor);  
+
+
 		IbusFactory.ibusSimpleHttp(misDatos.tiempo_hervor,
-															 misDatos.tipo_lupulo,
+															 misDatos.tipo_lupulo.id,
 															 misDatos.gramos_lupulo,
 															 misDatos.alfa_acidos,
 															 misDatos.litros_mosto_post_hervor,
